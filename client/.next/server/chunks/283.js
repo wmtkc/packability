97a313435671b11314284cf21d22c6947eb6b3a7
William Casey <will.casey5@gmail.com>
2022-02-20 23:28:01 -0700

    add build directory

---
+++ client/.next/server/chunks/283.js
@@ -0,0 +1,116 @@
+"use strict";
+exports.id = 283;
+exports.ids = [283];
+exports.modules = {
+
+/***/ 4283:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ Submit)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "styled-jsx/style"
+var style_ = __webpack_require__(9816);
+var style_default = /*#__PURE__*/__webpack_require__.n(style_);
+// EXTERNAL MODULE: external "@apollo/client"
+var client_ = __webpack_require__(9114);
+;// CONCATENATED MODULE: ./lib/mutations/createBook.ts
+
+const CREATE_BOOK_MUTATION = client_.gql`
+    mutation Mutation($title: String!, $author: String!) {
+        createBook(title: $title, author: $author) {
+            id
+            title
+            author
+        }
+    }
+`;
+
+;// CONCATENATED MODULE: ./components/Forms/BookSubmit.tsx
+
+
+
+
+function Submit() {
+    const [createBook1, { loading  }] = (0,client_.useMutation)(CREATE_BOOK_MUTATION);
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const formData = new window.FormData(form);
+        const title = formData.get('title');
+        const author = formData.get('author');
+        form.reset();
+        createBook1({
+            variables: {
+                title,
+                author
+            },
+            update: (cache, { data: { createBook  }  })=>{
+                cache.modify({
+                    fields: {
+                        books (existingBooks = []) {
+                            const newBookRef = cache.writeFragment({
+                                data: createBook,
+                                fragment: client_.gql`
+                  fragment NewBook on books {
+                    id
+                    type
+                  }
+                `
+                            });
+                            return [
+                                newBookRef,
+                                ...existingBooks
+                            ];
+                        }
+                    }
+                });
+            }
+        });
+    };
+    return(/*#__PURE__*/ (0,jsx_runtime_.jsxs)("form", {
+        onSubmit: handleSubmit,
+        className: "jsx-f42919216699f74d",
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                className: "jsx-f42919216699f74d",
+                /*#__PURE__*/ children: "Submit"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("input", {
+                placeholder: "title",
+                name: "title",
+                type: "text",
+                required: true,
+                className: "jsx-f42919216699f74d"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("input", {
+                placeholder: "author",
+                name: "author",
+                type: "author",
+                required: true,
+                className: "jsx-f42919216699f74d"
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("button", {
+                type: "submit",
+                disabled: loading,
+                className: "jsx-f42919216699f74d",
+                children: "Submit"
+            }),
+            jsx_runtime_.jsx((style_default()), {
+                id: "f42919216699f74d",
+                children: "form.jsx-f42919216699f74d{border-bottom:1px solid #ececec;\npadding-bottom:20px;\nmargin-bottom:20px}\nh1.jsx-f42919216699f74d{font-size:20px}\ninput.jsx-f42919216699f74d{display:block;\nmargin-bottom:10px}"
+            })
+        ]
+    }));
+};
+
+
+/***/ })
+
+};
+;
---
+++ client/.next/server/pages/ssr.js
@@ -0,0 +1,230 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 968;
+exports.ids = [968];
+exports.modules = {
+
+/***/ 4650:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "getServerSideProps": () => (/* binding */ getServerSideProps),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1523);
+/* harmony import */ var _components_InfoBox__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4939);
+/* harmony import */ var _components_Header__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(8969);
+/* harmony import */ var _components_Forms_BookSubmit__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(4283);
+/* harmony import */ var _components_BookList__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(3675);
+/* harmony import */ var _lib_queries_books__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(7503);
+/* harmony import */ var _lib_apolloClient__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(2724);
+
+
+
+
+
+
+
+
+const SSRPage = ()=>/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_components_App__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z, {
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Header__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {}),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_InfoBox__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {
+                children: "ℹ️ This page shows how to use SSR with Apollo."
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_Forms_BookSubmit__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {}),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_BookList__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z, {})
+        ]
+    })
+;
+async function getServerSideProps() {
+    const apolloClient = (0,_lib_apolloClient__WEBPACK_IMPORTED_MODULE_7__/* .initializeApollo */ ["in"])();
+    await apolloClient.query({
+        query: _lib_queries_books__WEBPACK_IMPORTED_MODULE_6__/* .ALL_BOOKS_QUERY */ .k,
+        variables: _lib_queries_books__WEBPACK_IMPORTED_MODULE_6__/* .allBooksQueryVars */ .X
+    });
+    return (0,_lib_apolloClient__WEBPACK_IMPORTED_MODULE_7__/* .addApolloState */ ._p)(apolloClient, {
+        props: {}
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SSRPage);
+
+
+/***/ }),
+
+/***/ 9114:
+/***/ ((module) => {
+
+module.exports = require("@apollo/client");
+
+/***/ }),
+
+/***/ 7596:
+/***/ ((module) => {
+
+module.exports = require("@apollo/client/utilities");
+
+/***/ }),
+
+/***/ 6330:
+/***/ ((module) => {
+
+module.exports = require("deepmerge");
+
+/***/ }),
+
+/***/ 113:
+/***/ ((module) => {
+
+module.exports = require("lodash/isEqual");
+
+/***/ }),
+
+/***/ 4558:
+/***/ ((module) => {
+
+module.exports = require("next/config");
+
+/***/ }),
+
+/***/ 562:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 4014:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 8020:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 9565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 4365:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-middleware-regex.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 1292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 6052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 4226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 5052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 1853:
+/***/ ((module) => {
+
+module.exports = require("next/router");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 9816:
+/***/ ((module) => {
+
+module.exports = require("styled-jsx/style");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [190,676,664,72,964,724,283], () => (__webpack_exec__(4650)));
+module.exports = __webpack_exports__;
+
+})();
