e5994e5f0ade957cdafa910192d1900297d286de
William Casey <will.casey5@gmail.com>
2022-02-27 14:43:45 -0700

    removed all style elements

--- src/components/rich-text/code.tsx
+++ src/components/rich-text/code.tsx
@@ -1,6 +1,8 @@
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx'
 
+import styles from '@styles/components/code.module.css'
+
 const Code = ({
     children,
     language = 'javascript',
@@ -10,33 +12,19 @@ const Code = ({
 }) => {
     return (
         <>
-            <pre>
+            <pre className={styles.pre}>
                 <code
+                    className={styles.code}
                     dangerouslySetInnerHTML={{
                         __html: Prism.highlight(
                             children,
                             Prism.languages[language.toLowerCase()] ||
                                 Prism.languages.javascript,
+                            language,
                         ),
                     }}
                 />
             </pre>
-
-            <style jsx>{`
-                pre {
-                    tab-size: 2;
-                }
-
-                code {
-                    overflow: auto;
-                    display: block;
-                    padding: 0.8rem;
-                    line-height: 1.5;
-                    background: #f5f5f5;
-                    font-size: 0.75rem;
-                    border-radius: var(--radius);
-                }
-            `}</style>
         </>
     )
 }
